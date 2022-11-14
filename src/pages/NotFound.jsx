b691f07bc9407b68ae70c2cbe6a12c2743aff53d
Orangel Brito <orangeleando@gmail.com>
2022-11-14 14:19:48 -0500

    Se agregaron las paginas , containers y componentes de acuerdo a la metodologia Atomic Desing

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import '../styles/NotFound.scss'
 
 const NotFound = () => {
-    return (
-        <div>
-            Not Found, Error 404.
-        </div>
-    );
+	return (
+		<div className='content'>
+			<div className='content__img'>
+				<img src='404.png' alt='nave-404' />
+				<p className='content__number'>
+						404
+				</p>
+			</div>
+			<div className='content__description'>
+				<p className='content__error'>
+					UPSSSS!! Something wrong!, Page Not Found.
+				</p>
+				<a href="/" className='content__button'>Get me out of Here!</a>
+			</div>
+		</div>
+
+	);
 }
 
 export default NotFound;
