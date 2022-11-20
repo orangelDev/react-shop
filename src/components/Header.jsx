3ce76c5c0e51049625051608b49b86713c618b5a
Orangel Brito <orangeleando@gmail.com>
2022-11-19 20:36:33 -0500

    Agregamos otro estado para el manejo del checkout y modificamos sus styles,tambien usamos useContext para el componente MyOrder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,13 @@ import '@styles/Header.scss'
 import Menu from '@components/Menu'
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
+import MyOrder from '../containers/MyOrder'
 import AppContext from "../context/AppContext";
 import shoppingCart from '@icons/icon_shopping_cart.svg';
 
 const Header = () => {
 const [toggle,setToggle] = useState(false)
+const [toggleOrders, setToggleOrders] = useState(false)
 const {state} = useContext(AppContext)
 
 const handleToggle = () => {
@@ -44,13 +46,17 @@ const handleToggle = () => {
           <li className="navbar-email" onClick={handleToggle}>
             platzi@example.com
           </li>
-          <li className="navbar-shopping-cart">
+          <li 
+            className="navbar-shopping-cart" 
+            onClick={() => setToggleOrders(!toggleOrders)}
+          >
             <img src={shoppingCart} alt="shopping cart" />
             {state.cart.length > 0 ? <div>{state.cart.length }</div> : null}
           </li>
         </ul>
       </div>
       {toggle && <Menu />}
+      {toggleOrders && <MyOrder />}
     </nav>
   );
 };
