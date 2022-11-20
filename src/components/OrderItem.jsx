420cee560d65bc4e6a9915fefee456a65179d958
Orangel Brito <orangeleando@gmail.com>
2022-11-19 21:52:58 -0500

    Eliminando productos del carrito

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
 import '@styles/OrderItem.scss';
 
 import close from '@icons/icon_close.png'
-const OrderItem = ({ product }) => {
+
+const OrderItem = (props) => {
+	
+	const { product, indexValue } = props
+	const { removeFromCart } = React.useContext(AppContext)
+
+	const handleRemove = (index) => {
+		removeFromCart(index)
+	}
 	return (
 		<div className="OrderItem">
 			<figure>
@@ -10,9 +19,14 @@ const OrderItem = ({ product }) => {
 			</figure>
 			<p>{product.title}</p>
 			<p>{product.price}</p>
-			<img src={close} alt="close" />
+			<img 
+				src={close} 
+				alt="close" 
+				onClick={() => handleRemove(indexValue )}
+			/>
 		</div>
 	);
+
 }
 
 export default OrderItem;
--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -13,9 +13,17 @@ const useInitialState =() => {
             cart: [...state.cart, payload]
         });
     }
+
+		const removeFromCart = (indexValue) => {
+			setState({
+				...state,
+				cart: state.cart.filter((_,index) => index !== indexValue),
+			})
+		}
     return {
         state,
-        addToCart
+        addToCart,
+				removeFromCart
     }
 }
 
